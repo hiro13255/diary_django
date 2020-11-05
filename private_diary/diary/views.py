8eae0b52e47ebba76b561d92ec63d7b396f4c308
rain1275 <kirakira7573@gmail.com>
2020-11-05 23:28:03 +0900

    フォーム動作時の処理　追記

--- private_diary/diary/views.py
+++ private_diary/diary/views.py
@@ -1,6 +1,12 @@
+import logging
+
+from django.urls import reverse_lazy
+
 from django.views import generic
 
 from .forms import InquiryForm
+
+logger = logging.getLogger(__name__) # ロガー取得
 
 # Create your views here.
 class IndexView(generic.TemplateView):
@@ -9,3 +15,9 @@ class IndexView(generic.TemplateView):
 class InquiryView(generic.FormView):
     template_name = 'inquiry.html'
     form_class = InquiryForm
+    success_url = reverse_lazy('diary:inquiry') #指定URLにリダイレクト
+
+    def form_valid(self, form):
+        form.send_email() # メール送信メソッド
+        logger.info('Inquiry sent by {}'.format(form.cleaned_data['name'])) # ビューからログを出力
+        return super().form_valid(form)
